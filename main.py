385fc383f15bf4916991e9b84ecaccd1334d1a75
AnJW-HGU <jingglenonggang@gmail.com>
2026-01-07 00:21:21 +0900

    doc: Edit gitignore

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ if __name__ == "__main__":
     degradation()
     print("-----pairdata-----------")
     generate_meta_info_pairdata()
-    torch.cuda.empty_cache()
     print("-----train--------------")
     train_E2E()
     print("-----test---------------")
